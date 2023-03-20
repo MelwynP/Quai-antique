19d8e5b5202ce3eac693dad1223d6240048540d6
Perrin Melwyn <melwyn_438@hotmail.com>
2023-03-20 14:55:53 +0100

    form the booking

---
+++ src/Form/BookFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateReservation')
+            ->add('hourReservation')
+            ->add('numberPeople')
+            ->add('allergy')
+            ->add('user')
+            ->add('restaurant')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
